3c8eba6cb05fe87e0d3c48a0123613dd537d407f
feliciar <feliciar@kth.se>
2017-06-30 10:12:01 +0200

    Verify that g = -(Y-P)' equals gi = -(Yi-pi)'

--- BackwardPass.m
+++ BackwardPass.m
@@ -3,8 +3,7 @@
 function BackwardPass (H, Y, P, RNN)
     t = size(H,2);
     % gt is the derivate of L in respect of o
-    g = -(Y-P)'; % TODO Is this the same as gi = -(y-p)' ?
-
+    g = -(Y-P)'; 
     gradV = g'*H';
 
 end
